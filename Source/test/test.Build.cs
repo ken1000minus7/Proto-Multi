8a7956f05c2463d82c604e6c2807fad935cbaa21
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-01-19 21:04:17 +0530

    beginning

---
+++ Source/test/test.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class test : ModuleRules
+{
+	public test(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		MinFilesUsingPrecompiledHeaderOverride = 1;
+		bFasterWithoutUnity = true;
+	}
+}
